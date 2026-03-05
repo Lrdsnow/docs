4609d81cf1fa607b16aa391cd00735f18aaf271d
lrdsnow <Lrdsnow101@gmail.com>
2026-03-04 16:13:15 -0800

    docs!

---
+++ src/lib/layout.shared.tsx
@@ -0,0 +1,17 @@
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+
+// fill this with your actual GitHub info, for example:
+export const gitConfig = {
+  user: 'lrdsnow',
+  repo: 'docs',
+  branch: 'main',
+};
+
+export function baseOptions(): BaseLayoutProps {
+  return {
+    nav: {
+      title: 'Lrdsnow Docs',
+    },
+    githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
+  };
+}
